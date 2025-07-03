b151ab64018441020795ba1471cd318a09c2a251
Vitor Bansen - Mailou.Cloud <vitor.bansen@mailou.com.br>
2025-07-03 10:31:16 -0300

    mudança axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  typeof window === 'undefined'
+    ? process.env.NEXT_PUBLIC_API_URL // SSR
+    : process.env.NEXT_PUBLIC_API_URL || '/api'; // client-side fallback
+
 const api = axios.create({
-  baseURL: '/api', 
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
