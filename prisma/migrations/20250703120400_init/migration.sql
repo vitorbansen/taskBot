9d080d8b622e6205e059cffb3c1190f41a04bf25
Vitor Bansen - Mailou.Cloud <vitor.bansen@mailou.com.br>
2025-07-03 09:08:17 -0300

    conectando ao banco

--- prisma/migrations/20250703120400_init/migration.sql
+++ prisma/migrations/20250703120400_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "Robot" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "startTime" TEXT NOT NULL,
     "endTime" TEXT NOT NULL,
     "color" TEXT NOT NULL,
     "manual" BOOLEAN NOT NULL,
-    "day" INTEGER NOT NULL
+    "day" INTEGER NOT NULL,
+
+    CONSTRAINT "Robot_pkey" PRIMARY KEY ("id")
 );
